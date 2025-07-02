858f5f47aafc9b29c687aae7105a77c5cd0e2ca2
Sahil khandelwal <81964222+Nishu2000-hub@users.noreply.github.com>
2025-07-02 08:48:46 +0530

    Delete modernwebdev-main directory

--- modernwebdev-main/src/environments.js
+++
@@ -1,5 +0,0 @@
-export const env_vars = {
-    applicationId : "S8od3uPzNhKfctY2Xao0eoizRQhaLMeYJ22GRx1M",
-    javaScriptKey : "RJ1bzvgskO2gyP5WRE8iIPEnUCckeGffuadvopJx",
-    serverURL : "https://parseapi.back4app.com"
-};
